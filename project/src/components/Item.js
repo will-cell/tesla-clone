899bf30bc22db1a2fc9e97b6bc0bb9d5435aa6aa
will-cell <williamh.henri@yahoo.fr>
2021-01-16 15:29:51 +0100

    added item et fonts

---
+++ project/src/components/Item.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Item.css'
+
+function Item() {
+    return (
+        <div className='item' style={{
+            
+        }}>
+            
+        </div>
+    )
+}
+
+export default Item
